2f86dd590311a6da87e2db70c0069fb10fb22190
Michael Morgenbesser <web@werk.wien>
2026-04-24 09:37:38 +0200

    refactor(Q-H1): also collapse EventReadCommand into AbstractModelReadCommand

--- src/Command/EventReadCommand.php
+++ src/Command/EventReadCommand.php
@@ -3,35 +3,11 @@
 namespace Webwerkwien\ContaoCliBridgeBundle\Command;
 
 use Contao\CalendarEventsModel;
-use Contao\CoreBundle\Framework\ContaoFramework;
 use Symfony\Component\Console\Attribute\AsCommand;
-use Symfony\Component\Console\Input\InputArgument;
 
 #[AsCommand(name: 'contao:event:read', description: 'Read a Contao calendar event as JSON')]
-class EventReadCommand extends AbstractReadCommand
+class EventReadCommand extends AbstractModelReadCommand
 {
-    public function __construct(private readonly ContaoFramework $framework)
-    {
-        parent::__construct();
-    }
-
-    protected function configure(): void
-    {
-        $this->addArgument('id', InputArgument::REQUIRED, 'Event ID');
-    }
-
-    protected function doExecute(): int
-    {
-        $this->framework->initialize();
-
-        $id    = (int) $this->input->getArgument('id');
-        $event = CalendarEventsModel::findById($id);
-
-        if ($event === null) {
-            return $this->outputError("Event not found: $id");
-        }
-
-        $this->outputRecord($event->row());
-        return Command::SUCCESS;
-    }
+    protected function modelClass(): string { return CalendarEventsModel::class; }
+    protected function entityName(): string { return 'Event'; }
 }
